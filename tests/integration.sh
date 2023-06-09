e3b9fa7262edd2fa983ca34ce1913c5fe10c7930
RohanSasne <rohansasne30@gmail.com>
2023-06-09 12:18:00 +0530

    Ad

--- tests/integration.sh
+++ tests/integration.sh
@@ -1,28 +1,3 @@
 #!/bin/bash
 
-NAME=${1:-$HOSTNAME}
-
-set -o pipefail
-
-cd $( dirname $0 )
-for f in ${NAME}.*.xml; do
-    rm -rf pecan output.log
-    Rscript --vanilla ../web/workflow.R --settings $f 2>&1 | tee output.log
-    if [ $? -ne 0 ]; then
-        echo "----------------------------------------------------------------------"
-        echo "TEST $f FAILED (RETURN CODE != 0)"
-        echo "----------------------------------------------------------------------"
-        exit -1
-    elif [ $(grep -v DONE pecan/STATUS | wc -l) -ne 0 ]; then
-        echo "----------------------------------------------------------------------"
-        echo "TEST $f FAILED (ERROR IN STATUS)"
-        echo "----------------------------------------------------------------------"
-        cat pecan/STATUS
-        exit -1
-    else
-        echo "----------------------------------------------------------------------"
-        echo "TEST $f OK"
-        echo "----------------------------------------------------------------------"
-    fi
-    rm -rf output.log pecan
-done
+curl -H "Host: pecan.localhost" http://localhost/pecan/01-introduction.php
